62a33baa0f8c7b1313b250e9c02aa7801cb5c2f5
kusuma_.__ <rhshah199@gmail.com>
2025-07-20 09:24:43 -0700

    added pausable and events

--- src/mycoin.sol
+++ src/mycoin.sol
@@ -3,13 +3,45 @@ pragma solidity ^0.8.20;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/security/Pausable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
 contract MyCoin is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+
+    event TokensMinted(address indexed to, uint256 amount);
+    event TokensBurned(address indexed from, uint256 amount);
+
+    constructor(uint256 initialSupply) ERC20("MyCoin", "MYC") {
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) external OnlyOwner {
         _mint(to, amount);
+        emit TokensMinted(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+        emit TokensBurned(from, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) 
+        internal
+        override
+        whenNotPaused
+        {
+            super._beforeTokenTransfer(from, to, amount);
+        }
+
+    function snapshot() external onlyOwner {
+        _snapshot();
     }
 }
